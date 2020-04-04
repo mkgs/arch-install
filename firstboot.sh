b32827511a6d762e40c27e1d987adc7e6d8d1088
Michael Gisi <gisi@umich.edu>
2020-04-04 15:37:18 -0400

    fix paths

--- firstboot.sh
+++ firstboot.sh
@@ -8,24 +8,23 @@ ssh-keygen
 
 echo "Getting config files..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mkdir ~/.config && cd ~/.config
-git clone https://github.com/mkgs/dotfiles.git
+git clone https://github.com/mkgs/dotfiles.git ~/.config/dotfiles
 echo "Copying vim config..."
-cp dotfiles/.vimrc ../
-cp -r dotfiles/.vim ../
+cp ~/.config/dotfiles/.vimrc ~/
+cp -r ~/.config/dotfiles/.vim ~/
 echo "Copying i3 config..."
-cp -r dotfiles/.config/i3 ./
-cp -r dotfiles/.config/i3status ./
+cp -r ~/.config/dotfiles/.config/i3 ~/
+cp -r ~/.config/dotfiles/.config/i3status ~/
 read -p "i3 gaps? (y/n): " I3_GAPS
 if [ $I3_GAPS = "n" ]; then
-    sed -i 's/gaps inner 45/gaps inner 0/' i3/config
+    sed -i 's/gaps inner 45/gaps inner 0/' ~/.config/i3/config
 fi
 echo "Copying bash config..."
-cp dotfiles/.bash_profile ../
-cp dotfiles/.bashrc ../
-cp dotfiles/.xinitrc ../
-cp dotfiles/runhack ../
-chmod +x ../runhack
+cp ~/.config/dotfiles/.bash_profile ~/
+cp ~/.config/dotfiles/.bashrc ~/
+cp ~/.config/dotfiles/.xinitrc ~/
+cp ~/.config/dotfiles/runhack ~/
+chmod +x ~/runhack
 
 echo "Installing Heroku CLI..."
 sudo npm install -g heroku
