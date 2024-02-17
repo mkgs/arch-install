4c8816f8b3d0f19ea5acb1057b97f6760dad970c
Michael Gisi <gisi@umich.edu>
2024-02-16 21:18:57 -0500

    remove gaps config, fix clone dir

--- firstboot.sh
+++ firstboot.sh
@@ -5,17 +5,14 @@ ssh-keygen
 
 echo "Getting config files..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/mkgs/dotfiles.git ~/code
+git clone https://github.com/mkgs/dotfiles.git ~/code/dotfiles
 echo "Copying vim config..."
 cp ~/code/dotfiles/.vimrc ~/
 cp -r ~/code/dotfiles/.vim ~/
 echo "Copying i3 config..."
 cp -r ~/code/dotfiles/.config/i3 ~/.config/
 cp -r ~/code/dotfiles/.config/i3status ~/.config/
-read -p "i3 gaps? (y/n): " I3_GAPS
-if [ $I3_GAPS = "n" ]; then
-    sed -i 's/gaps inner 45/gaps inner 0/' ~/.config/i3/config
-fi
+
 echo "Copying bash config..."
 cp ~/code/dotfiles/.bash_profile ~/
 cp ~/code/dotfiles/.bashrc ~/
